464b9ad26abcb25b62fc501f8d8a92fe4f1a8e35
brimdor <chrisnelsonx@gmail.com>
2024-10-16 22:00:12 -0500

    Update set-hostname.sh

--- macos/set-hostname.sh
+++ macos/set-hostname.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
-#
-# This corrects a shitty point of confusion with macOS where if you bounce
-# between wireless and wired connections, macOS will suddenly throw up its hands
-# and add a random-ass number to your hostname. Do it a couple times and you're
-# in like, the thousands appended to your hostname, which makes you look like a
-# chump when your machine is called "incredible-programmer-9390028", like
-# you're behind 9,390,027 other better programmers before you. Sheesh.
-#
-# Anyway, this runs in `dot` and only asks for your permission (usually TouchID)
-# if it actually needs to change your hostname for you, otherwise it's fast to
-# toss into `dot` anyway.
-#
-# None of this really matters in the big scheme of things, but it bothered me.
 
 hostname=$(scutil --get LocalHostName)
 
